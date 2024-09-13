32d4d45131255726c773aaa189977638b9830370
John Greenwell <johnmgreenwell@gmail.com>
2024-09-12 21:25:08 -0500

    Add bluetooth disablement/enablement

---
+++ peripheral/bluetooth-off.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+sudo systemctl mask bluetooth
+sudo systemctl disable bluetooth
+
+#EOF
---
+++ peripheral/bluetooth-on.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+sudo systemctl unmask bluetooth
+sudo systemctl enable bluetooth
+
+#EOF
