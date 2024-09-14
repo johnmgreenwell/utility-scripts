fd4aeeea005d2abf36d3caa907f068f9351545d7
John Greenwell <johnmgreenwell@gmail.com>
2024-09-13 21:31:10 -0500

    Update ssh-init to first install prereq openssh-client

--- init/ssh-init.sh
+++ init/ssh-init.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
-# Install openssh server and let it through the firewall
+# Install openssh clinet and server and allow it through the firewall
 
 if which ssh >/dev/null 2>&2; then
   echo "OpenSSH is already installed."
 else
   echo "Installing OpenSSH."
+  sudo apt update
+  sudo apt install openssh-client -y
   sudo apt install openssh-server -y
 fi
 
