23d91fe39c06b7ec6b9a1bd0df7af7f798ed6e09
John Greenwell <johnmgreenwell@gmail.com>
2024-09-15 21:26:24 -0500

    Add rdp-init script

---
+++ init/rdp-init.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# Install and setup xrdp
+
+if which xrdp >/dev/null 2>&2; then
+  echo "XRDP is already installed."
+else
+  echo "Installing XRDP."
+  sudo apt update
+  sudo apt install xrdp
+  sudo systemctl enable xrdp
+  sudo systemctl start xrdp
+  echo "gnome-session" > ~/.xsession
+  sudo adduser xrdp ssl-cert
+fi
+
+if which xrdp >/dev/null 2>&2; then
+  exit 0
+else
+  echo "Failed to install XRDP."
+  exit 1
+fi
+
+# EOF
