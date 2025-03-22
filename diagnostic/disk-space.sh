986ce1cef878d1f6474c8293cccea0afc7c2463a
John Greenwell <johnmgreenwell@gmail.com>
2025-03-22 18:50:00 -0500

    Correct brief desciption header

--- diagnostic/disk-space.sh
+++ diagnostic/disk-space.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Check the remaining disk space for root partition in bytes
+# Return the remaining disk space for root partition in bytes
 # Usage: disk-space.sh
 
 echo $(df --output=avail / | tail -n 1)
