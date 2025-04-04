da8e0db46544ab543404a368d04fe529867f9a64
John Greenwell <johnmgreenwell@gmail.com>
2025-04-04 13:13:14 -0500

    Expand simple time-get feature to include options

--- diagnostic/time-get.sh
+++ diagnostic/time-get.sh
@@ -1,7 +1,24 @@
 #!/bin/bash
 # Return a string of the current time UTC
-# Usage: time-get.sh
+# Usage: time-get.sh [short|full|iso|epoch]
 
-echo "$(date -u +"%Y-%m-%d %H:%M:%S UTC")"
+case "$1" in
+    short)
+        date -u +"%H:%M:%S"
+        ;;
+    full|"")
+        date -u +"%Y-%m-%d %H:%M:%S UTC"
+        ;;
+    iso)
+        date -u +"%Y-%m-%dT%H:%M:%SZ"
+        ;;
+    epoch)
+        date -u +"%s"
+        ;;
+    *)
+        echo "Usage: $0 [short|full|iso|epoch]"
+        exit 1
+        ;;
+esac
 
 # EOF
