44db56e7992a3c5e500e71d046dfb8f4c473b9cd
John Greenwell <johnmgreenwell@gmail.com>
2025-04-14 22:23:08 -0500

    Add simple variable volume set script

---
+++ peripheral/volume-set.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# Usage: volume-set.sh [VOLUME_PERCENTAGE]
+
+if [ -z "$1" || ]; then
+    echo "Usage: $0 <VOLUME_PERCENTAGE>"
+    exit 1
+fi
+
+case "$1" in
+    ("" | *[!0-9]*)
+        echo "Error: input must be positive decimal integer number." >&2
+        exit 1
+esac
+
+if [ "$1" -lt 1 ] || [ "$1" -gt 100 ]; then
+    echo "Error: input must be in range 0-100." >&2
+    exit 1
+fi
+
+amixer -D pulse sset Master $1%
+
+#EOF
