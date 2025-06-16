2c977ddb53b74389b79e38bfffc625a5a9a33de7
John Greenwell <johnmgreenwell@gmail.com>
2025-06-16 18:22:01 -0500

    Add clarification to some text, abbreviate some conditionals, reorganize some in udev-rule script

--- general/create-udev-rule.sh
+++ general/create-udev-rule.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 # Create a new udev rule for a particular peripheral attachment by name
 # Requires bus and device numbers (e.g. via lsusb) of intended peripheral,
-# as well as the intended udev enforced name (e.g. 'camera_1' etc.)
+# as well as the intended udev enforced name (e.g. 'cam_1' etc.)
 # Usage: ./create-udev-rule.sh <BUS_NUM> <DEVICE_NUM> <DEVICE_NAME>
+
+RULE_FILE="/etc/udev/rules.d/99-custom.rules"
 
 [ "$#" -eq 3 ] || { echo "Usage: $0 <BUS_NUM> <DEVICE_NUM> <DEVICE_NAME>"; exit 1; }
 
@@ -11,22 +13,16 @@ DEVICE_NUM=$2
 DEVICE_NAME=$3
 
 DEVICE_PATH=$(lsusb -s "$BUS_NUM:$DEVICE_NUM" | awk '{print $6}')
-if [ -z "$DEVICE_PATH" ]; then
-    echo "Device $BUS_NUM:$DEVICE_NUM not found."
-    exit 1
-fi
+[ -z "$DEVICE_PATH" ] && { echo "Device $BUS_NUM:$DEVICE_NUM not found."; exit 1; }
 
 SYSFS_PATH=$(find /sys/bus/usb/devices/usb*/ -name "*$DEVICE_PATH")
-if [ -z "$SYSFS_PATH" ]; then
-    echo "Could not locate sysfs path for device $DEVICE_PATH."
-    exit 1
-fi
+[ -z "$SYSFS_PATH" ] && { echo "Could not locate sysfs path for device $DEVICE_PATH."; exit 1; }
 
 ID_VENDOR=$(udevadm info -a -p "$SYSFS_PATH" | grep -m1 "ATTRS{idVendor}" | awk -F'==' '{print $2}' | tr -d '" ')
 ID_PRODUCT=$(udevadm info -a -p "$SYSFS_PATH" | grep -m1 "ATTRS{idProduct}" | awk -F'==' '{print $2}' | tr -d '" ')
 SERIAL=$(udevadm info -a -p "$SYSFS_PATH" | grep -m1 "ATTRS{serial}" | awk -F'==' '{print $2}' | tr -d '" ')
 
-RULE_FILE="/etc/udev/rules.d/99-custom.rules"
+echo "Using $RULE_FILE..."
 echo "Creating udev rule..."
 echo "SUBSYSTEM==\"usb\", ATTR{idVendor}==\"$ID_VENDOR\", ATTR{idProduct}==\"$ID_PRODUCT\", ATTR{serial}==\"$SERIAL\", SYMLINK+=\"${DEVICE_NAME}\"" | sudo tee -a "$RULE_FILE" > /dev/null
 echo "Reloading udev rules..."
@@ -34,10 +30,6 @@ sudo udevadm control --reload-rules
 sudo udevadm trigger
 echo "Udev rule added. Device accessible at /dev/$DEVICE_NAME."
 
-if [ -e "/dev/$DEVICE_NAME" ]; then
-    echo "/dev/$DEVICE_NAME successfully created!"
-else
-    echo "/dev/$DEVICE_NAME not yet created. Try unplugging and replugging the device."
-fi
+[ -e "/dev/$DEVICE_NAME" ] && echo "/dev/$DEVICE_NAME successfully created!" || echo "/dev/$DEVICE_NAME not yet created. Please check device and try again."
 
 # EOF
