1fd87bdc3c4c46fdff4ff624cb30d0014f971847
John Greenwell <johnmgreenwell@gmail.com>
2025-06-21 17:40:38 -0500

    Add more options to simple time string rendering script

--- diagnostic/time-get.sh
+++ diagnostic/time-get.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Return a string of the current time UTC
-# Usage: time-get.sh [short|full|iso|epoch]
+# Usage: time-get.sh [short|full|iso|us|eu|epoch|human]
 
 case "$1" in
     short)
@@ -12,11 +12,20 @@ case "$1" in
     iso)
         date -u +"%Y-%m-%dT%H:%M:%SZ"
         ;;
+    us)
+        date -u +"%m/%d/%Y %I:%M:%S %p"
+        ;;
+    eu)
+        date -u +"%d/%m/%Y %H:%M:%S"
+        ;;
     epoch)
         date -u +"%s"
         ;;
+    human)
+        date -u +"%I:%M %p on %m/%d/%Y"
+        ;;
     *)
-        echo "Usage: $0 [short|full|iso|epoch]"
+        echo "Usage: $0 [short|full|iso|us|eu|epoch|human]"
         exit 1
         ;;
 esac
