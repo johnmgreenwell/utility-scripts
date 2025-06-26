44c1f35ae3dbe67cca57f9a16f58f740615f3269
John Greenwell <johnmgreenwell@gmail.com>
2025-06-26 15:55:41 -0500

    Add exit status to net-check script

--- diagnostic/net-check.sh
+++ diagnostic/net-check.sh
@@ -2,6 +2,6 @@
 # Check if the primary network is up or down
 # Usage: net-check.sh
 
-ip route | grep -q "default" && echo "UP" || echo "DOWN"
+ip route | grep -q "default" && { echo "UP"; exit 0; } || { echo "DOWN"; exit 1; }
 
 # EOF
