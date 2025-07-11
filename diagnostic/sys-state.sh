16cca2ee4f89e516d37d7c6788b65332755e62a3
John Greenwell <johnmgreenwell@gmail.com>
2025-07-11 17:42:21 -0500

    Reformat output in sys-state script

--- diagnostic/sys-state.sh
+++ diagnostic/sys-state.sh
@@ -2,9 +2,10 @@
 # Return a set of strings representing simple system state
 # Usage: sys-state.sh
 
-echo "=== System Uptime ==="; uptime
-echo "=== Memory Usage ==="; free -h
-echo "=== Disk Usage ==="; df -h /
+echo "====== System Uptime ======"; uptime
+echo "====== Memory Usage ======="; free -h
+echo "======= Disk Usage ========"; df -h /
 echo "=== Top 5 CPU Processes ==="; ps -eo pid,ppid,cmd,%mem,%cpu --sort=-%cpu | head -n 6
+echo "==========================="
 
 # EOF
