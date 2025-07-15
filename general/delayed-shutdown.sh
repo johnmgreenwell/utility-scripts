0f117a3652641b4743d9bb882c03bbc67b6aed92
John Greenwell <johnmgreenwell@gmail.com>
2025-07-15 17:18:24 -0500

    Add argument validity check to delayed-shutdown script

--- general/delayed-shutdown.sh
+++ general/delayed-shutdown.sh
@@ -2,10 +2,9 @@
 # Shutdown the machine after a set number of minutes
 # Usage: delayed-shutdown.sh [MINUTES]
 
-[ -z "$1" ] && { echo "Usage: $0 <minutes>"; exit 1; }
+[[ -z "$1" || ! "$1" =~ ^[0-9]+$ ]] && { echo "Usage: $0 <minutes>"; exit 1; }
 
 MINUTES=$1
-
 echo "System will shut down in $MINUTES minute(s)..."
 sudo shutdown -h +$MINUTES
 
