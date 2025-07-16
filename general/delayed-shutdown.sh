7090cb27ea6c0467255918346387cf1ffc3f2687
John Greenwell <johnmgreenwell@gmail.com>
2025-07-16 14:41:39 -0500

    Add privileges check to delayed suspend/shutdown scripts

--- general/delayed-shutdown.sh
+++ general/delayed-shutdown.sh
@@ -2,6 +2,12 @@
 # Shutdown the machine after a set number of minutes
 # Usage: delayed-shutdown.sh [MINUTES]
 
+if [ "$EUID" -ne 0 ]; then
+    echo "This script requires root privileges to access some udev rules."
+    echo "Please run with sudo or as root."
+    exit 1
+fi
+
 [[ -z "$1" || ! "$1" =~ ^[0-9]+$ ]] && { echo "Usage: $0 <minutes>"; exit 1; }
 
 MINUTES=$1
