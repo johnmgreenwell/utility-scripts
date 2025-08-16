e0eb5ed3e29792aa4e11c9d1025a7042a0ec3f12
John Greenwell <johnmgreenwell@gmail.com>
2025-08-16 14:25:00 -0500

    Add rudimentary sanity check for portability

--- diagnostic/disk-space.sh
+++ diagnostic/disk-space.sh
@@ -2,6 +2,7 @@
 # Return the remaining disk space for root partition in bytes
 # Usage: disk-space.sh
 
+command -v df >/dev/null 2>&1 || { echo "Command 'df' not found."; exit 1; }
 echo $(df --output=avail / | tail -n 1)
 
 # EOF
