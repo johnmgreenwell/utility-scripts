033c420157beb34fb5c889421b16ea83c43ee4f4
John Greenwell <johnmgreenwell@gmail.com>
2025-09-03 11:15:55 -0500

    Add minor robustness touches to simple wifi scripts

--- peripheral/wifi-off.sh
+++ peripheral/wifi-off.sh
@@ -2,7 +2,10 @@
 # Disable wifi
 # Usage: wifi-off.sh
 
+command -v nmcli >/dev/null 2>&1 || { echo "Command 'nmcli' not found."; exit 1; }
 sudo nmcli radio wifi off
 echo "dtoverlay=disable-wifi" | sudo tee -a /boot/firmware/config.txt > /dev/null
+
+exit 0
 
 # EOF
--- peripheral/wifi-on.sh
+++ peripheral/wifi-on.sh
@@ -2,7 +2,10 @@
 # Enable wifi
 # Usage: wifi-on.sh
 
+command -v nmcli >/dev/null 2>&1 || { echo "Command 'nmcli' not found."; exit 1; }
 sudo sed -i '/dtoverlay=disable-wifi/d' /boot/firmware/config.txt
 sudo nmcli radio wifi on
+
+exit 0
 
 # EOF
