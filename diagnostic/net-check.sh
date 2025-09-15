0cbb23b3aaabb3ec25609c64a9c2d86cc4fd5228
John Greenwell <johnmgreenwell@gmail.com>
2025-09-15 17:27:27 -0500

    Add minor robustness and format changes to net check script

--- diagnostic/net-check.sh
+++ diagnostic/net-check.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
-# Check if the primary network is up or down
+# Check if the primary network is up or down, exits 0 for network up
 # Usage: net-check.sh
 
-ip route | grep -q "default" && { echo "UP"; exit 0; } || { echo "DOWN"; exit 1; }
+command -v ip >/dev/null 2>&1 || { echo "Command 'ip' not found."; exit 2; }
+ip route | grep -q "default" && { echo "Network: UP."; } || { echo "DOWN"; exit 1; }
+ping -q -c 1 -W 1 8.8.8.8 >/dev/null && echo "Internet: UP." || echo "Internet: DOWN"
+
+exit 0
 
 # EOF
