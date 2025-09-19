0dc947896dca73a4b59992e8356c7c866be9dc74
John Greenwell <johnmgreenwell@gmail.com>
2025-09-19 17:19:09 -0500

    Rearrange arguments and make device optional in samba init script, make minor improvements to formatting

--- init/samba-init.sh
+++ init/samba-init.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
-# Install smba server and prepare its configuration and auto-start
+# Install samba server and prepare its configuration and auto-start
 # to a directory called 'shared/' in MOUNT_DIRECTORY
-# Usage: ./samba-init.sh <DEVICE_FILE> <MOUNT_DIRECTORY> <USERNAME>
+# Usage: ./samba-init.sh [USERNAME] [MOUNT_DIRECTORY] <DEVICE_FILE>
+USER="${1:-pi}"
+MOUNT="${2:-/mnt/hdd1}"
+DEVICE="$3"
 
-DEVICE=${1:-"/dev/sda1"}
-MOUNT=${2:-"/mnt/hdd1"}
-USER=${3:-"pi"}
-
-if which samba >/dev/null 2>&2; then
+if which samba >/dev/null 2>&1; then
   echo "Samba is already installed."
   exit 0
 else
@@ -15,32 +14,30 @@ else
   sudo apt install samba -y
 fi
 
-sudo mkdir -p $MOUNT/shared
-
+sudo mkdir -p "$MOUNT/shared"
 echo "Updating smb.conf file with custom settings..."
 read -r -d '' SETUP << EOM
 # Place custom server details here
 # https://ubuntu.com/tutorials/install-and-configure-samba#3-setting-up-samba
 EOM
-echo "$SETUP" | sudo tee -a '/etc/samba/smb.conf' > /dev/null
+echo "$SETUP" | sudo tee -a /etc/samba/smb.conf > /dev/null
 
-# Custom device and mount string as array
-STARTUP=(sudo mount "$DEVICE" "$MOUNT")
-
-echo "Adding auto-mount on start..."
-if [ ! -f /etc/rc.local ]; then
-  echo "#!/bin/bash" | sudo tee '/etc/rc.local' > '/dev/null'
-  sudo chmod +x /etc/rc.local
+if [ -n "$DEVICE" ]; then
+  echo "Adding auto-mount on start..."
+  if [ ! -f /etc/rc.local ]; then
+    echo "#!/bin/bash" | sudo tee /etc/rc.local > /dev/null
+    sudo chmod +x /etc/rc.local
+  fi
+  echo "sudo mount \"$DEVICE\" \"$MOUNT\"" | sudo tee -a /etc/rc.local > /dev/null
+  # Mount the drive
+  sudo mount "$DEVICE" "$MOUNT"
 fi
-echo "${STARTUP[@]}" | sudo tee -a '/etc/rc.local' > '/dev/null'
 
 # Setup a user/password to access the samba share
-sudo smbpasswd -a $USER
+sudo smbpasswd -a "$USER"
 
-# Mount the drive and start the server
-"${STARTUP[@]}"
+# Start the server
 sudo systemctl restart smbd.service nmbd.service
-
 exit 0
 
 # EOF
