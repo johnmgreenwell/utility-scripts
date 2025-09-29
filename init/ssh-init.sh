98a40872d2e4db2d62edbbaaf950c19071b6459a
John Greenwell <johnmgreenwell@gmail.com>
2025-09-29 14:13:52 -0500

    Add robustness checks for ssh installation script

--- init/ssh-init.sh
+++ init/ssh-init.sh
@@ -2,16 +2,16 @@
 # Install openssh clinet and server and allow it through the firewall
 # Usage: ssh-init.sh
 
-if which ssh >/dev/null 2>&2; then
+if which ssh >/dev/null 2>&1; then
   echo "OpenSSH is already installed."
 else
   echo "Installing OpenSSH."
-  sudo apt update
-  sudo apt install openssh-client -y && sudo apt install openssh-server -y
+  sudo apt update || { echo "Failed to update apt."; exit 1; }
+  sudo apt install openssh-client -y && sudo apt install openssh-server -y || { echo "Failed to install openssh."; exit 1; }
 fi
 
 sudo ufw allow ssh
-sudo ufw allow 1022/tcp comment 'Open port ssh TCP/1022 as failsafe for upgrades'
+sudo ufw allow 1022/tcp comment 'Open port ssh TCP/1022 as failsafe for upgrades.'
 sudo systemctl restart ssh
 
 exit 0
