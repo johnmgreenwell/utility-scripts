3f900eb5fee2b7941f6e665fcf58d384e3069c6b
John Greenwell <johnmgreenwell@gmail.com>
2025-09-30 20:21:18 -0500

    Clarify language in error messaging

--- init/ssh-init.sh
+++ init/ssh-init.sh
@@ -6,7 +6,7 @@ if which ssh >/dev/null 2>&1; then
   echo "OpenSSH is already installed."
 else
   echo "Installing OpenSSH."
-  sudo apt update || { echo "Failed to update apt."; exit 1; }
+  sudo apt update || { echo "Failed to update. Exiting..."; exit 1; }
   sudo apt install openssh-client -y && sudo apt install openssh-server -y || { echo "Failed to install openssh."; exit 1; }
 fi
 
