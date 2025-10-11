646e6cd55377945567a0c2cd6a8cf9df13ffc113
John Greenwell <johnmgreenwell@gmail.com>
2025-10-11 14:17:23 -0500

    Add privilege check and info echo on success for bluetooth scripts

--- peripheral/bluetooth-off.sh
+++ peripheral/bluetooth-off.sh
@@ -2,8 +2,14 @@
 # Disable bluetooth
 # Usage: bluetooth-off.sh
 
+[ $(id -u) -ne 0 ] && { echo "This script requires admin privileges."; exit 1; }
+
 sudo systemctl disable bluetooth
 sudo systemctl mask bluetooth
 echo "dtoverlay=disable-bt" | sudo tee -a /boot/firmware/config.txt > /dev/null
+
+echo "Bluetooth enabled. Restart may be required."
+
+exit 0
 
 # EOF
--- peripheral/bluetooth-on.sh
+++ peripheral/bluetooth-on.sh
@@ -2,8 +2,14 @@
 # Enable bluetooth
 # Usage: bluetooth-on.sh
 
+[ $(id -u) -ne 0 ] && { echo "This script requires admin privileges."; exit 1; }
+
 sudo sed -i '/dtoverlay=disable-bt/d' /boot/firmware/config.txt
 sudo systemctl unmask bluetooth
 sudo systemctl enable bluetooth
+
+echo "Bluetooth disabled. Restart may be required."
+
+exit 0
 
 # EOF
