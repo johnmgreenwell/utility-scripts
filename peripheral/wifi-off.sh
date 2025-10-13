7d4b9199402545f5114572b5db4a145fa47d0e70
John Greenwell <johnmgreenwell@gmail.com>
2025-10-13 18:33:05 -0500

    Add privilege check and info echo on success for wifi scripts

--- peripheral/wifi-off.sh
+++ peripheral/wifi-off.sh
@@ -2,9 +2,13 @@
 # Disable wifi
 # Usage: wifi-off.sh
 
-command -v nmcli >/dev/null 2>&1 || { echo "Command 'nmcli' not found."; exit 1; }
+[ $(id -u) -ne 0 ] && { echo "This script requires admin privileges."; exit 1; }
+
+command -v nmcli >/dev/null 2>&1 || { echo "Command 'nmcli' not found."; exit 2; }
 sudo nmcli radio wifi off
 echo "dtoverlay=disable-wifi" | sudo tee -a /boot/firmware/config.txt > /dev/null
+
+echo "Wifi disabled. Restart may be required."
 
 exit 0
 
--- peripheral/wifi-on.sh
+++ peripheral/wifi-on.sh
@@ -2,9 +2,13 @@
 # Enable wifi
 # Usage: wifi-on.sh
 
-command -v nmcli >/dev/null 2>&1 || { echo "Command 'nmcli' not found."; exit 1; }
+[ $(id -u) -ne 0 ] && { echo "This script requires admin privileges."; exit 1; }
+
+command -v nmcli >/dev/null 2>&1 || { echo "Command 'nmcli' not found."; exit 2; }
 sudo sed -i '/dtoverlay=disable-wifi/d' /boot/firmware/config.txt
 sudo nmcli radio wifi on
+
+echo "Wifi enabled. Restart may be required."
 
 exit 0
 
