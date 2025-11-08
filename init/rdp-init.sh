52cb75137c1e5a28e1ebdd3c5fa4618d93d1d050
John Greenwell <johnmgreenwell@gmail.com>
2025-11-07 19:52:53 -0600

    Reorder post-installation actions and add privileges check in rdp-init script

--- init/rdp-init.sh
+++ init/rdp-init.sh
@@ -2,16 +2,18 @@
 # Install and setup xrdp
 # Usage: rdp-init.sh
 
+[ $(id -u) -ne 0 ] && { echo "This script requires admin privileges."; exit 1; }
+
 if which xrdp >/dev/null 2>&1; then
   echo "XRDP is already installed."
 else
   echo "Installing XRDP."
   sudo apt update || { echo "Failed to update. Exiting..."; exit 1; }
   sudo apt install xrdp -y || { echo "Failed to install xrdp."; exit 1; }
-  sudo systemctl enable xrdp
-  sudo systemctl start xrdp
   echo "gnome-session" > ~/.xsession
   sudo adduser xrdp ssl-cert
+  sudo systemctl enable xrdp
+  sudo systemctl start xrdp
 fi
 
 which xrdp >/dev/null 2>&2 && { sudo ufw allow 3389; exit 0; } || { echo "Failed to install XRDP."; exit 1; }
