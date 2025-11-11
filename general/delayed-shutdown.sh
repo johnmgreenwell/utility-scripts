8d5d8de668279da0a7960a53d3b71e1ccce6b286
John Greenwell <johnmgreenwell@gmail.com>
2025-11-11 15:46:01 -0600

    Abbreviate privileges check to one-liner in delayed-shutdown script

--- general/delayed-shutdown.sh
+++ general/delayed-shutdown.sh
@@ -2,11 +2,7 @@
 # Shutdown the machine after a set number of minutes
 # Usage: delayed-shutdown.sh [MINUTES]
 
-if [ "$EUID" -ne 0 ]; then
-    echo "This script requires root privileges to access some udev rules."
-    echo "Please run with sudo or as root."
-    exit 1
-fi
+[ $(id -u) -ne 0 ] && { echo "This script requires admin privileges."; exit 1; }
 
 [[ -z "$1" || ! "$1" =~ ^[0-9]+$ ]] && { echo "Usage: $0 <minutes>"; exit 1; }
 
