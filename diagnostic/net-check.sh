6fae69ce13a9f895da09e6c2d8a613f8e944eed8
John Greenwell <johnmgreenwell@gmail.com>
2025-11-26 13:16:12 -0600

    Correct typo in net-check script results reporting

--- diagnostic/net-check.sh
+++ diagnostic/net-check.sh
@@ -3,7 +3,7 @@
 # Usage: net-check.sh
 
 command -v ip >/dev/null 2>&1 || { echo "Command 'ip' not found."; exit 2; }
-ip route | grep -q "default" && { echo "Network: UP."; } || { echo "DOWN"; exit 1; }
+ip route | grep -q "default" && { echo "Network: UP."; } || { echo "Network: DOWN"; exit 1; }
 ping -q -c 1 -W 1 8.8.8.8 >/dev/null && echo "Internet: UP." || echo "Internet: DOWN"
 
 exit 0
