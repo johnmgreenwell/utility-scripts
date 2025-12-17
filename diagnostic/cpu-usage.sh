7fe999236eb7cdb045e7a007a8427f128e205c4b
John Greenwell <johnmgreenwell@gmail.com>
2025-12-16 21:19:27 -0600

    Fix command use to collect current cpu usage rather than average, add some minor improvements and such

--- diagnostic/cpu-usage.sh
+++ diagnostic/cpu-usage.sh
@@ -2,6 +2,10 @@
 # Return the current CPU usage percentage
 # Usage: cpu-usage.sh
 
-echo $(top -bn1 | grep "Cpu(s)" | awk '{print $2 + $4}')
+command -v mpstat >/dev/null 2>&1 && { mpstat 1 1 | awk '/Average/ {print 100 - $NF}'; exit 0; }
+echo $(top -bn2 | grep "Cpu(s)" | tail -1 | awk '{print 100 - $8}')
+#echo "$(top -bn2 | grep "Cpu(s)" | tail -1 | awk '{printf "%.1f", 100 - $8}')%"
+
+exit 0
 
 # EOF
