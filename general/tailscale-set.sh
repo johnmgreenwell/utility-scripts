21b161e9f9eb2020eccd88b2688ac76ca40932a9
John Greenwell <johnmgreenwell@gmail.com>
2026-02-05 12:21:04 -0600

    Fix error from template copy, make status a valid argument rather than a fallback

--- general/tailscale-set.sh
+++ general/tailscale-set.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Starts or stops the tailscale service
-# Usage: tailscale [start|stop]
+# Usage: tailscale [start|stop|status]
 
 [ $(id -u) -ne 0 ] && { echo "This script requires admin privileges."; exit 1; }
 command -v systemctl >/dev/null 2>&1 || { echo "Command 'systemctl' not found."; exit 2; }
@@ -11,20 +11,20 @@ if [[ -n $1 ]]; then
     sudo systemctl start tailscaled
   elif [[ $1 == "stop" ]]; then
     sudo systemctl stop tailscaled
+  elif [[ $1 == "status" ]]
+    STATUS=$(systemctl is-active tailscaled)
+    if [ "$STATUS" = "active" ]; then
+      TIMESTAMP=$(systemctl show tailscaled --property=ActiveEnterTimestamp --value)
+    else
+      TIMESTAMP=$(systemctl show tailscaled --property=InactiveEnterTimestamp --value)
+    fi
+    echo "Tailscale: $STATUS since $TIMESTAMP."
   else
     echo "Invalid argument."
     exit 4
   fi
 else
-  echo "Checking status of tailscale..."
-  STATUS=$(systemctl is-active <service_name>)
-  if [ "$STATUS" = "active" ]; then
-      TIMESTAMP=$(systemctl show <service_name> --property=ActiveEnterTimestamp --value)
-  else
-      TIMESTAMP=$(systemctl show <service_name> --property=InactiveEnterTimestamp --value)
-  fi
-  echo "Tailscale: $STATUS since $TIMESTAMP."
-  echo "Usage: $0 [start|stop]"
+  echo "Usage: $0 [start|stop|status]"
 fi
 
 exit 0
