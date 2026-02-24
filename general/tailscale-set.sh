c45d85d749abb2602e99a3ab9cb8a3624d63763a
John Greenwell <johnmgreenwell@gmail.com>
2026-02-24 07:05:47 -0500

    Refactor such that usage format check is applied first, if-switch slightly simplified in tailscale-set script

--- general/tailscale-set.sh
+++ general/tailscale-set.sh
@@ -2,29 +2,26 @@
 # Starts or stops the tailscale service
 # Usage: tailscale [start|stop|status]
 
-[ $(id -u) -ne 0 ] && { echo "This script requires admin privileges."; exit 1; }
-command -v systemctl >/dev/null 2>&1 || { echo "Command 'systemctl' not found."; exit 2; }
-systemctl cat tailscaled &>/dev/null || { echo "Service 'tailscaled' not found."; exit 3; }
+[ $# -eq 0 ] && { echo "Usage: $0 {start|stop|status}"; exit 1; }
+[ $(id -u) -ne 0 ] && { echo "This script requires admin privileges."; exit 2; }
+command -v systemctl >/dev/null 2>&1 || { echo "Command 'systemctl' not found."; exit 3; }
+systemctl cat tailscaled &>/dev/null || { echo "Service 'tailscaled' not found."; exit 4; }
 
-if [[ -n $1 ]]; then
-  if [[ $1 == "start" ]]; then
-    sudo systemctl start tailscaled
-  elif [[ $1 == "stop" ]]; then
-    sudo systemctl stop tailscaled
-  elif [[ $1 == "status" ]]
-    STATUS=$(systemctl is-active tailscaled)
-    if [ "$STATUS" = "active" ]; then
-      TIMESTAMP=$(systemctl show tailscaled --property=ActiveEnterTimestamp --value)
-    else
-      TIMESTAMP=$(systemctl show tailscaled --property=InactiveEnterTimestamp --value)
-    fi
-    echo "Tailscale: $STATUS since $TIMESTAMP."
+if [[ $1 == "start" ]]; then
+  sudo systemctl start tailscaled
+elif [[ $1 == "stop" ]]; then
+  sudo systemctl stop tailscaled
+elif [[ $1 == "status" ]]
+  STATUS=$(systemctl is-active tailscaled)
+  if [ "$STATUS" = "active" ]; then
+    TIMESTAMP=$(systemctl show tailscaled --property=ActiveEnterTimestamp --value)
   else
-    echo "Invalid argument."
-    exit 4
+    TIMESTAMP=$(systemctl show tailscaled --property=InactiveEnterTimestamp --value)
   fi
+  echo "Tailscale: $STATUS since $TIMESTAMP."
 else
-  echo "Usage: $0 [start|stop|status]"
+  echo "Invalid argument."
+  exit 4
 fi
 
 exit 0
