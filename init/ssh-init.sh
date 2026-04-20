5ec9272b4d2e53109ea63a210a762d83cd18ae39
John Greenwell <johnmgreenwell@gmail.com>
2026-04-20 17:06:38 -0400

    Add optional port assignment allowance to ufw while removing the default for security

--- init/ssh-init.sh
+++ init/ssh-init.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # Install openssh clinet and server and allow it through the firewall
-# Usage: ssh-init.sh
+# Usage: ssh-init.sh <PORT_NUM>
+
+PORT_NUM="${1:-22}"
 
 [ $(id -u) -ne 0 ] && { echo "This script requires admin privileges."; exit 1; }
 
@@ -12,9 +14,12 @@ else
   sudo apt install openssh-client -y && sudo apt install openssh-server -y || { echo "Failed to install openssh."; exit 3; }
 fi
 
-sudo ufw allow ssh
-sudo ufw allow 1022/tcp comment 'Open port ssh TCP/1022 as failsafe for upgrades.'
+echo "Setting SSH firewall port allowance to $PORT_NUM..."
+sudo ufw delete allow 22/tcp
+sudo ufw allow $PORT_NUM/tcp
 sudo systemctl restart ssh
+
+Echo "Operation complete."
 
 exit 0
 
