7070d7f5aa94def6719e021ff1a3ed0b73b7d173
John Greenwell <johnmgreenwell@gmail.com>
2026-05-16 19:43:08 -0400

    Refactor cpu-usage script for clarity with checks

--- diagnostic/cpu-usage.sh
+++ diagnostic/cpu-usage.sh
@@ -2,8 +2,14 @@
 # Return the current CPU usage percentage
 # Usage: cpu-usage.sh
 
-command -v mpstat >/dev/null 2>&1 && { mpstat 1 1 | awk '/Average/ {print 100 - $NF}'; exit 0; }
-echo $(top -bn2 | grep "Cpu(s)" | tail -1 | awk '{print 100 - $8}')
+if command -v mpstat >/dev/null 2>&1; then
+  mpstat 1 1 | awk '/Average/ {print 100 - $NF}';
+elif command -v top >/dev/null 2>&1; then
+  echo $(top -bn2 | grep "Cpu(s)" | tail -1 | awk '{print 100 - $8}')
+else
+  echo "Neither commands 'mpstat' nor 'df' found."
+  exit 1
+fi
 
 exit 0
 
