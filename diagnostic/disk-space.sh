7e613c954d5c2e90c0c2e8f1bffc08f40ed5d65a
John Greenwell <johnmgreenwell@gmail.com>
2026-05-16 19:43:28 -0400

    Refactor disk-space script for clarity with checks

--- diagnostic/disk-space.sh
+++ diagnostic/disk-space.sh
@@ -2,7 +2,13 @@
 # Return the remaining disk space for root partition in bytes
 # Usage: disk-space.sh
 
-command -v df >/dev/null 2>&1 || { echo "Command 'df' not found."; exit 1; }
-echo $(df --output=avail / | tail -n 1)
+if command -v df >/dev/null 2>&1; then
+  echo $(df --output=avail / | tail -n 1)
+else
+  echo "Command 'df' not found."
+  exit 1
+fi
+
+exit 0
 
 # EOF
