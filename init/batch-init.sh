0fbe27554f3386921665d3fcc2b8035e35d5a551
John Greenwell <johnmgreenwell@gmail.com>
2026-06-20 18:13:33 -0400

    Rewrite arguments check for batch-init script

--- init/batch-init.sh
+++ init/batch-init.sh
@@ -5,7 +5,7 @@
 APP_LIST=$1
 
 [ $(id -u) -ne 0 ] && { echo "This script requires admin privileges."; exit 1; }
-[[ -z $APP_LIST ]] && { echo "No app list provided. Exiting..."; exit 2; }
+[ "$#" -eq 1 ] || { echo "Usage: $0 [APP_LIST_FILE]"; exit 2; }
 [[ ! -f "$APP_LIST" ]] && { echo "App list file \"$APP_LIST\" not found. Exiting..."; exit 3; }
 
 echo "Updating apt..."
