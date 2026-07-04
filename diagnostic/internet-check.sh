36b53637ef2c0527661589cf655897e441f75ba4
John Greenwell <johnmgreenwell@gmail.com>
2026-07-03 20:47:17 -0400

    Add color code output to internet-check script, requires checking downstream result

--- diagnostic/internet-check.sh
+++ diagnostic/internet-check.sh
@@ -6,8 +6,8 @@ TIMESTAMP=$(date '+%Y-%m-%d %H:%M:%S')
 TARGET="8.8.8.8"
 
 if command -v ping >/dev/null 2>&1; then
-  ping -q -c 1 -W 1 $TARGET > /dev/null 2>&1 && STATUS="Online" || STATUS="Offline"
-  echo "[$TIMESTAMP] Network Status: $STATUS"
+  ping -q -c 1 -W 1 $TARGET > /dev/null 2>&1 && STATUS="Online" && COLOR="\e[32m" || STATUS="Offline" && COLOR="\e[31m"
+  echo -e "[$TIMESTAMP] Network Status: ${COLOR}${STATUS}\e[0m"
   [ "$STATUS" = "Online" ] || exit 1
 else
   echo "Command 'ping' not found."
@@ -15,5 +15,3 @@ else
 fi
 
 exit 0
-
-# EOF
