f83fd18313b4e7334957c469cbb8de68a8d14f6a
John Greenwell <johnmgreenwell@gmail.com>
2026-09-24 23:58:47 -0400

    Refactor indentation size in repos-update script

--- general/repos-update.sh
+++ general/repos-update.sh
@@ -5,9 +5,9 @@
 command -v find >/dev/null 2>&1 || { echo "Command 'find' not found."; exit 1; }
 
 find . -type d -name .git -not -path '*/\.git/*' -prune | while read -r GITDIR; do
-    repo=$(dirname "$GITDIR")
-    echo "Pulling: $REPO"
-    (cd "$REPO" && git pull --ff-only) && echo " ...success." || echo " ...failed."
+  repo=$(dirname "$GITDIR")
+  echo "Pulling: $REPO"
+  (cd "$REPO" && git pull --ff-only) && echo " ...success." || echo " ...failed."
 done
 
 echo "Repository pulls completed."
